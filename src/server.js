55ffa8ce062bfc5fa07cba3904fcb899de81e072
selee <tldms296@gmail.com>
2023-02-09 14:15:48 +0900

    challenge middleware

--- src/server.js
+++ src/server.js
@@ -4,16 +4,44 @@ const PORT = 4000;
 
 const app = express();
 
-const hondleHome = (req, res) => {
-    return res.sendfile(__dirname + "/home.html");
+const methodLogger = (req, res, next) => {
+    console.log("METHOD:", req.method);
+    next();
 }
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
+const routerLogger = (req, res, next) => {
+    console.log("PATH:", req.path);
+    next();
 }
+const timeLogger = (req, res, next) => {
+    const date = new Date();
+    console.log("Time:", `${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()}`);
+    next();
+}
+const securityLogger = (req, res, next) => {
+    if(req.protocol === 'https') {
+        console.log("secure");
+    } else {
+        console.log("insecure");
+    }
+    next();
+}
+const protectorMiddleware = (req, res, next) => {
+    if(req.path === '/protected') {
+        return res.send("protected page");
+    } 
+    next();
+}
+const home = (req, res) => {
+    console.log("I will respond.");
+    return res.send("hello");
+}
+const login = (req, res) => {
+    return res.send("login");
+}
+app.use(methodLogger, routerLogger, timeLogger, securityLogger, protectorMiddleware);
+app.get("/", home);
+app.get("/login", login);
 
-app.get("/", hondleHome);
-app.get("/login", handleLogin)
-
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
