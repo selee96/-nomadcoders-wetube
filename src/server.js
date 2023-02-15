7bcb8252fa5461788b99cc056f05c6b52e89edcf
selee <tldms296@gmail.com>
2023-02-16 08:58:21 +0900

    controller 구분

--- src/server.js
+++ src/server.js
@@ -1,46 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const methodLogger = (req, res, next) => {
-    console.log("METHOD:", req.method);
-    next();
-}
-const routerLogger = (req, res, next) => {
-    console.log("PATH:", req.path);
-    next();
-}
-const timeLogger = (req, res, next) => {
-    const date = new Date();
-    console.log("Time:", `${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()}`);
-    next();
-}
-const securityLogger = (req, res, next) => {
-    if(req.protocol === 'https') {
-        console.log("secure");
-    } else {
-        console.log("insecure");
-    }
-    next();
-}
-const protectorMiddleware = (req, res, next) => {
-    if(req.path === '/protected') {
-        return res.send("protected page");
-    } 
-    next();
-}
-const home = (req, res) => {
-    console.log("I will respond.");
-    return res.send("hello");
-}
-const login = (req, res) => {
-    return res.send("login");
-}
-app.use(methodLogger, routerLogger, timeLogger, securityLogger, protectorMiddleware);
-app.get("/", home);
-app.get("/login", login);
+app.use(logger)
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
